eefa67ac42802886cbe5be6ee291cc23653fd739
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:32:32 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+let contar = 5
+for (let índice = 0; índice <= 4; índice++) {
+    basic.showNumber(contar)
+    basic.pause(1000)
+    contar += -1
+}
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(5000)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
